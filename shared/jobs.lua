c766f91de3cbba3c5d9fb7338ab3822622862ac3
Smithy <96345069+Smitthyy@users.noreply.github.com>
2022-12-03 20:53:53 +0000

    added strawberry bennys job

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -734,4 +734,33 @@ QBShared.Jobs = {
             },
         },
     },
+    ['strawberrybennys'] = {
+        label = 'Strawberry Modifications Shop',
+        defaultDuty = false,
+        grades = {
+            ['0'] = {
+                name = 'Recruit',
+                payment = 50
+            },
+            ['1'] = {
+                name = 'Mechanic',
+                payment = 70
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = false,
+                payment = 100
+            },
+            ['3'] = {
+                name = 'Co-Owner',
+                isboss = true,
+                payment = 120
+            },
+            ['4'] = {
+                name = 'Owner',
+                isboss = true,
+                payment = 120
+            },
+        },
+    },
 }
