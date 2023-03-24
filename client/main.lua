6bd9ba669b61f5b7fc7487534cc3c0aae5506fe7
Smithy <96345069+Smitthyy@users.noreply.github.com>
2023-03-24 22:26:51 +0000

    Disable controller aim assist

--- client/main.lua
+++ client/main.lua
@@ -13,6 +13,19 @@ end)
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Disable auto aim
+CreateThread(function()-- probably just add this to an existing script
+    while true do
+        Wait(500)-- can set to 0 if needed 
+        local gamepad = GetLastInputMethod(2)
+        if not gamepad then
+           SetPlayerTargetingMode(3)--set targeting to "free-aim" if a controller is being used
+        else
+            Wait(2000)--wait if the player is using a keyboard
+        end
+    end
+end)
+
 
             -- Jobs Creator integration (jobs_creator)
             RegisterNetEvent("jobs_creator:injectJobs", function(jobs)
